3398e5bff25c9e0db7c594f346c1605205c63bed
gala377 <r.galczak@gmail.com>
2020-05-19 22:30:32 +0000

    Fixed private.predictOne function so it actually now works

--- code/localLinear.r
+++ code/localLinear.r
@@ -1,17 +1,12 @@
-source("./getKnn.r")
-
 # Prywatna funkcja dokonująca predykcji jak localLinear dla pojedyńczej wartości.
 private.predictOne <- function(train, x, formula, n, dist_method="euclidean") {
   nn_train <- getKnn(train, x, n, dist_method)
   model <- lm(formula, data=nn_train)
   # this gives us a warning but its fine
-  predict(model, x)[[1]]
+  predict(model, data.frame(as.list(x)))[[1]]
 }
 
-# doest this even work? 
-# It will probably break getKnn because it weren't supposed to be used with dataframes.
-
-# Dokonuje predykcji wartości dla zbioru `test`.
+# Dokonuje predykcji wartości dla zbioru `test` (który nie powienien mieć kolumny z wartością przewidywaną).
 # Model lokalny tworzony jest dla `n` najbliższych sąsiadów licząc dystanś metodą `dist_method`
 # ze zbioru `train`.
 # Model liniowy tworzony jest dla wybranego modelu lokalnego z użyciem formuły `formula`.
