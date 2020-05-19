9e06d7156e8e8d4da452e43b72125f52d0f10e77
gala377 <r.galczak@gmail.com>
2020-05-19 20:53:03 +0000

    Created getKnn function

---
+++ code/getKnn.r
@@ -0,0 +1,12 @@
+# Resturns matrix containing n nearest neighbours of sample x
+# from dataset train using dist_method to compute distances.
+getKnn <- function(train, x, n, dist_method="euclidean") {
+    train_with_x <- rbind(x, train, deparse.level=0)
+    distances <- dist(train_with_x, dist_method)
+    distances <- as.matrix(distances)[1, ] # distences from the first row to the ech
+    distances <- distances[-1] # skip the first distance to itself
+    names(distances) <- strtoi(names(distances))-1 # get indexes in the train set
+    distances <- sort(distances)
+    n_indexes <- strtoi(names(distances)[1:n])
+    train[n_indexes, ]
+}
