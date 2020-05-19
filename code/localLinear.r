6af7d14563ef2290e31b39cf46d2b38abc43369f
gala377 <r.galczak@gmail.com>
2020-05-19 22:01:55 +0000

    Added documentation to localLinear functions

--- code/localLinear.r
+++ code/localLinear.r
@@ -1,5 +1,6 @@
 source("./getKnn.r")
 
+# Prywatna funkcja dokonująca predykcji jak localLinear dla pojedyńczej wartości.
 private.predictOne <- function(train, x, formula, n, dist_method="euclidean") {
   nn_train <- getKnn(train, x, n, dist_method)
   model <- lm(formula, data=nn_train)
@@ -9,6 +10,11 @@ private.predictOne <- function(train, x, formula, n, dist_method="euclidean") {
 
 # doest this even work? 
 # It will probably break getKnn because it weren't supposed to be used with dataframes.
+
+# Dokonuje predykcji wartości dla zbioru `test`.
+# Model lokalny tworzony jest dla `n` najbliższych sąsiadów licząc dystanś metodą `dist_method`
+# ze zbioru `train`.
+# Model liniowy tworzony jest dla wybranego modelu lokalnego z użyciem formuły `formula`.
 localLinear <- function(train, test, formula, n, dist_method="euclidean") {
     apply(test, 1, function(row) private.predictOne(train, row, formula, n, dist_method))
 }
