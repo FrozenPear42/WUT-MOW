85ffcd3f1e1f9725c04e847b292255a6af367aed
gala377 <r.galczak@gmail.com>
2020-05-31 13:21:50 +0200

    Added general wrapping function for linear regression alghorithms

--- code/getKnn.r
+++ code/getKnn.r
@@ -1,8 +1,9 @@
-# Resturns matrix containing n nearest neighbours of sample x
-# from dataset train using dist_method to compute distances.
+#' Resturns matrix containing `n` nearest neighbours of sample `x`
+#' from dataset `train` using builtin `dist` method with 
+#' `dist_method` to compute distances.
 getKnn <- function(train, x, n, dist_method="euclidean") {
     train_with_x <- rbind(x, train, deparse.level=0)
-    distances <- dist(train_with_x, dist_method)
+    distances <- dist(train_with_x, method=dist_method)
     distances <- as.matrix(distances)[1, ] # distences from the first row to the ech
     distances <- distances[-1] # skip the first distance to itself
     names(distances) <- strtoi(names(distances))-1 # get indexes in the train set
--- code/localLinear.r
+++ code/localLinear.r
@@ -1,15 +1,48 @@
-# Prywatna funkcja dokonująca predykcji jak localLinear dla pojedyńczej wartości.
-private.predictOne <- function(train, x, formula, n, dist_method="euclidean") {
-  nn_train <- getKnn(train, x, n, dist_method)
-  model <- lm(formula, data=nn_train)
-  # this gives us a warning but its fine
-  predict(model, data.frame(as.list(x)))[[1]]
+# This module requires `rpart` library loaded for
+# the `regressionTree.wrap` function to work.
+
+private.wrapOne <- function(train, x, n, dist_method, func, ...) {
+    nn_train <- getKnn(train, x, n, dist_method)
+    model <- func(..., data=nn_train)
+    predict(model, data.frame(as.list(x)))[[1]]
 }
 
-# Dokonuje predykcji wartości dla zbioru `test` (który nie powienien mieć kolumny z wartością przewidywaną).
-# Model lokalny tworzony jest dla `n` najbliższych sąsiadów licząc dystanś metodą `dist_method`
-# ze zbioru `train`.
-# Model liniowy tworzony jest dla wybranego modelu lokalnego z użyciem formuły `formula`.
-localLinear <- function(train, test, formula, n, dist_method="euclidean") {
-    apply(test, 1, function(row) private.predictOne(train, row, formula, n, dist_method))
+#' Predict values for `test` based on the `train` set with
+#' function `func` calculating regression model on
+#' some local data of length `n` provided by KNN-algorithm
+#' with distance calculated using builtin `dist` 
+#' function with `dist_method` argument.
+#'
+#' The `func` will be called with `data` argument already provided
+#' based on the `train` set. Any additional arguments can be
+#' passed with `...` variadic argument.
+#' 
+#' If you are passing a formula then the `test` set should not
+#' have predicted column.
+#' 
+#' For usage examples look at implementation of
+#' `localLinear.wrap` and `refressionTree.wrap` functions.
+wrap <- function(train, test, n, dist_method, func, ...) {
+    apply(test, 1, function(row) private.wrapOne(train, row, n, dist_method, func, ...))
+}
+
+#' Calls `wrap` function with predefined arguments
+#' for the KNN-alghoritm (n = 100 and dist_method="euclidean".
+default.wrap <- function(train, test, func, ...) {
+    wrap(train, test, 100, "euclidean", func, ...)
+}
+
+#' Pedict values in `test` using `formula` with
+#' regression model beeing fit with builtin `lm` function.
+localLinear.wrap <- function(train, test, formula) {
+    default.wrap(train, test, lm, formula)
+}
+
+#' Pedict values in `test` using `formula` with
+#' regression model beeing fit with builtin `rpart`
+#' function from `rpart` package. 
+#' 
+#' As a method "anove" is passed to the `rpart` function.
+regressionTree.wrap <- function(train, test, formula) {
+    default.wrap(train, test, rpart, formula=formula, method="anova")
 }
